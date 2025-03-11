8a3e45082916e2b4ef250ab87c2062f5bd3abfba
Altaseb Chernet <altacher227@gmail.com>
2025-03-11 12:11:14 -0700

    Update simple_calculator.java

--- simple_calculator.java
+++ simple_calculator.java
@@ -3,19 +3,20 @@ import java.util.Scanner;
 class Calculator{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("enter the first number: ");
+        System.out.print("enter the first number: ");
         double num1 = scanner.nextDouble();
-        System.out.println("enter the Second number: ");
+        System.out.print("enter the second number: ");
         double num2 = scanner.nextDouble();
         double sum = num1 + num2;
-        double sub = num1 - num2;
-        double mul = num1 * num2;
-        double div = num1 / num2;
+        double difference = num1-num2;
+        double multiply = num1*num2;
+        double division = num1 / num2;
+        double remainder = num1 % num2;
         System.out.printf("sum: %f\n",sum);
-        System.out.printf("sub: %f\n",sub);  
-        System.out.printf("mul: %f\n",mul);
-        System.out.printf("div: %f\n",div);
+        System.out.printf("difference: %f\n",difference);
+        System.out.printf("multiply: %f\n",multiply);
+        System.out.printf("division: %f\n",division);
+        System.out.printf("remainder: %f\n",remainder);
         scanner.close();
-
     }
 }
